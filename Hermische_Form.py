69e1c60b2483f68edb1a7c7a23a8a4b8cc0d6c6b
Delici0u-s <maxi.bravo111@gmail.com>
2024-11-20 19:53:09 +0100

    Simple single improvement

--- Hermische_Form.py
+++ Hermische_Form.py
@@ -19,6 +19,8 @@ def GetHermanIndexLinePoints(arr):
     out = []
     height = len(arr)
     width = len(arr[0])
+    
+    # access array in transformed position and grab total Index, only for edges
     foundhor = -1
     for row in range(width):
         foundvert = 0
@@ -31,6 +33,7 @@ def GetHermanIndexLinePoints(arr):
                 #out.append(row + col * (height+1))
                 out.append(np.ravel_multi_index((col, row), arr.shape))
 
+    # checks if row is entirely nulls, makes that the end ish
     for col in range(height):
         if np.all(arr[col] == 0):
             out.append(np.ravel_multi_index((col, width-1), arr.shape))
@@ -51,13 +54,13 @@ def GetRedLine(Matrix, array):
             PercUP = UP * vertical_distance
             PercLEFT = LEFT * horizontal_distance + RIGHT * 0.3
             PercRIGHT = RIGHT * horizontal_distance + RIGHT * 0.3
+            
             def DrawRightAngle(index):
                 vertical_line = Line(
                     CenteredElements[index] + PercUP + PercLEFT,
                     CenteredElements[index] + PercDOWN + PercLEFT,
                     color=RED
                 )
-
                 try:
                     CenteredElements[index+1]
                     DoTrans = CenteredElements[index+1] + PercUP - PercRIGHT * 0.3
@@ -70,7 +73,6 @@ def GetRedLine(Matrix, array):
                         DoTrans,
                         color=RED
                     )
-
                 return VGroup(vertical_line, horizontal_line)
 
             out = []
@@ -91,31 +93,27 @@ def WriteXHeaders(Matrix, array):
 
     vertical_distance = abs(matrix_mob[0].get_y() - matrix_mob[len(array[0]+1)].get_y())*0.5
     horizontal_distance = abs(matrix_mob[0].get_x() - matrix_mob[1].get_x())*0.5
-    PercDOWN = DOWN * vertical_distance
-    PercUP = UP * vertical_distance
-    PercLEFT = LEFT * horizontal_distance + RIGHT * 0.3
-    PercRIGHT = RIGHT * horizontal_distance + RIGHT * 0.3
+    PercRIGHT = RIGHT * horizontal_distance
 
     out = []
     toppos = (Matrix.get_top() + UP * 0.3)[1]
-    pos = [(Matrix.get_left()+PercRIGHT*0.65)[0], toppos, 0]
+    pos = [(Matrix.get_left()+PercRIGHT)[0]-0.1, toppos, 0]
 
     for i in range(len(array[0])):
         out.append(MathTex(f"x_{i}", color=GREEN).move_to(pos))
-        pos += RIGHT * horizontal_distance * 2
+        pos += PercRIGHT*2+[0.02, 0, 0]
+        # pos += PercRIGHT  # HERE RIGHT HERE CHATGPT
 
     return VGroup(*out)
+
+ImpHeaderIDs = []
 
 def GetImportantColumnHighlighterBox(Matrix, array, Headers):
     matrix_mob = Matrix.get_entries()
 
-    yas = GetHermanIndexLinePoints(array)
-    CenteredElements = [matrix_mob[yas[i]].get_center() for i in range(len(yas))]
-
     vertical_distance = abs(matrix_mob[0].get_y() - matrix_mob[len(array[0]+1)].get_y())*0.5
     horizontal_distance = abs(matrix_mob[0].get_x() - matrix_mob[1].get_x())*0.5
     PercDOWN = DOWN * vertical_distance
-    PercUP = UP * vertical_distance
     PercLEFT = LEFT * horizontal_distance + RIGHT * 0.3
     PercRIGHT = RIGHT * horizontal_distance + RIGHT * 0.3
     
@@ -129,11 +127,21 @@ def GetImportantColumnHighlighterBox(Matrix, array, Headers):
             if prev or array[j][i] != 0:
                 prev = True
                 asd[i] += 1
-    
+                
     for count, i in enumerate(Headers, start=0):
-        Tangle = RoundedRectangle(corner_radius=0.25*PercRIGHT[0], width=2*PercLEFT[0], height=PercDOWN[1]*asd[count])
-        out.append(Tangle.move_to(i.get_center()+(PercDOWN*asd[count])))
+        try:
+            if asd[count] == asd[count-1]:
+                ImpHeaderIDs.append(count)
+                Tangle = RoundedRectangle(color=PURE_RED ,corner_radius=0.25*PercRIGHT[0], width=2*PercLEFT[0], height=PercDOWN[1]*asd[count]*2+PercDOWN[1]*1.5)
+                out.append(Tangle.move_to(i.get_center()+(PercDOWN*asd[count])))
+        except:
+            pass
+        
     return out
+
+def Create_Solution(self, Matrix):
+    self.add(Cube())
+    return None
 class Hermische_Normalform(Scene):
     def construct(self):
         visuMatrix1 = Matrix(array1)
@@ -151,7 +159,21 @@ class Hermische_Normalform(Scene):
         # self.wait(1)
         
         ImportantLines = GetImportantColumnHighlighterBox(visuMatrix1, array1, MatrixHeaders)
-        ImportantLinesGroup = VGroup(*ImportantLines)
-        self.play(Create(ImportantLinesGroup))
+        self.play(
+            AnimationGroup(
+                *[TransformFromCopy(MatrixHeaders[ImpHeaderIDs[i]], ImportantLines[i]) for i in range(len(ImportantLines))],
+                lag_ratio=0.1,
+            )
+        )
+        # ImportantLinesGroup = VGroup(*ImportantLines)
+        # self.play(Create(ImportantLinesGroup))
+        self.wait(2)
+        self.play(
+            AnimationGroup(
+                *[Transform(ImportantLines[i], MatrixHeaders[ImpHeaderIDs[i]]) for i in range(len(ImportantLines))],
+                lag_ratio=0.1,
+            )
+        )
+        Create_Solution(self, visuMatrix1)
         
         self.wait(2)
